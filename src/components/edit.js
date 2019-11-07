36d1ebd15592132ee8e8892cbedd36e534823d81
ADAM VARDEN - STUDENT <G00359605@gmit.ie>
2019-11-07 14:23:51 +0000

    first commit

---
+++ src/components/edit.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Edit extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Helllo from Edit Component</h1>
+
+            </div>
+
+        )
+    }
+}
+
+export default Edit;
